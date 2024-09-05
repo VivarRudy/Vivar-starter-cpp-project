e70e89c8875daf769f8df73ad236c7ff15ad6aab
Rudy Vivar <rudyvivar@mail.weber.edu>
2024-09-05 18:22:34 +0000

    after todays class

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 
+using namespace std;
 int main(){
     // TODO
-    std::cout << "Hello World!" << std::endl;
+    cout << "What score did you get for the Notebook1?";
+    double score;
+    cin >> score; 
+
+char grade;
+    if(score >= 9){
+        grade = 'A';
+    } else if (score >= 8) {
+        grade = 'B';
+    } else if  (score >= 7){
+        grade = 'C';
+    } else {
+        grade = 'D';
+    }
+
+    if('0'){
+        cout << "Must be true to get to here \n";
+    }
+
+    cout << "A score of " << score << " means " << grade << endl;
     
     return 0;
 }
